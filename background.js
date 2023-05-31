cc6a96a7265fb2ab02e82949016c42393a8298d8
Saurabh <saurabhverma234567@gmail.com>
2023-05-31 11:30:56 +0530

    background-color-change

---
+++ background.js
@@ -0,0 +1,19 @@
+// Javascript
+
+const btn = document.getElementById("button");
+const randomcolor = () => {
+    let val = "0123456789ABCDEF";
+    let cons = "#";
+
+    for (let i = 0; i < 6; i++) {
+        cons = cons + val [Math.floor(Math.random() * 16)];
+    }
+    return cons;
+};
+console.log(randomcolor());
+
+function changeRandomColor() {
+    document.body.style.backgroundColor = randomcolor();
+}
+
+btn.addEventListener("click", changeRandomColor);
